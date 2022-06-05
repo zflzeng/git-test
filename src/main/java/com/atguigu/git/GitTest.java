e870245c6b72b4cbc2177a4f252367d30adbf93d
flzeng <flzeng@qq.com>
2022-06-05 22:24:29 +0800

    first commit

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.atguigu.git;
+
+/**
+ * @author flzeng
+ * @create 2022-06-05 22:22
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello, git!");
+    }
+}
